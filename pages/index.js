a77795d251f8bfa7919918c349af7554c0e29674
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-05-05 01:11:12 +0300

    Add needed packages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import Head from 'next/head'
+import Uppy from '@uppy/core'
+import XHRUpload from '@uppy/xhr-upload'
+import { Dashboard } from '@uppy/react'
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+
+const uppy = new Uppy({
+  allowMultipleUploads: false,
+
+})
+	.use(XHRUpload, {
+		endpoint: 'https://api2.transloadit.com'
+	});
+
+uppy.on('complete', (result) => {
+	console.log('Upload complete! Weve uploaded these files: ', result.successfful)
+})
+
 export default function Home() {
   return (
-    <h1>Lodaboi</h1>
+    <div>
+      <Dashboard
+        proudlyDisplayPoweredByUppy={false}
+				uppy={uppy}
+			/>
+    </div>
   )
 }
