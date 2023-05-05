a6876a770427b3ffa0641e029f8444de460f6db2
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-05-05 22:17:08 +0300

    Add app logo

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Uppy from '@uppy/core'
 import XHRUpload from '@uppy/xhr-upload'
 import { Dashboard } from '@uppy/react'
+import Image from 'next/image';
 import '@uppy/core/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
 
 const uppy = new Uppy({
   allowMultipleUploads: false,
-
 })
 	.use(XHRUpload, {
 		endpoint: 'https://api2.transloadit.com'
@@ -19,9 +19,19 @@ uppy.on('complete', (result) => {
 export default function Home() {
   return (
     <div className='container'>
-      <h1 className='heading'>
+      <span className='appName'>
+      <Image
+      src="/enabled-logo.svg"
+      width={300}
+      height={100}
+      alt="enabled logo"
+    /> 
+    <h4 className='subAppName'>Summarization app</h4>
+      </span>
+      
+      <h3 className='heading'>
         Put here the pdf file you want to summarize.
-      </h1>
+      </h3>
       <Dashboard
         proudlyDisplayPoweredByUppy={false}
 				uppy={uppy}
