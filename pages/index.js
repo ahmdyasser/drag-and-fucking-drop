88d5f9a886f09fa811dd2d1c3d8bf2354bd7929f
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-05-07 23:31:32 +0300

    Indent code

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,14 @@ var isFileUploaded = false;
 const uppy = new Uppy({
   allowMultipleUploads: false,
 })
-	.use(XHRUpload, {
-		endpoint: 'https://api2.transloadit.com'
-	});
+  .use(XHRUpload, {
+    endpoint: 'https://api2.transloadit.com'
+  });
 
 function Summarization() {
   const [isLoading, setIsLoading] = useState(false);
   uppy.on('complete', (result) => {
-    let timerId = setInterval(async function() {
+    let timerId = setInterval(async function () {
       if (await fetchStatus()) {
         clearInterval(timerId);
         setIsLoading(false);
@@ -28,53 +28,53 @@ function Summarization() {
     console.log('Upload complete! Weve uploaded these files: ', result.successfful);
     isFileUploaded = true;
   })
-    
+
   async function fetchStatus() {
     var time;
     let response = await fetch("https://reqres.in/api/users", {
-    method: "POST",
-    body: JSON.stringify({
-      name: "morpheus",
-      job: "leader"
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-  
-  const jsonResponse = await response.json()
-  time = jsonResponse["createdAt"];
-  const minutes = time.split('T')[1].split(':')[1]
-  console.log(jsonResponse);
-  console.log(minutes);
-  
+      method: "POST",
+      body: JSON.stringify({
+        name: "morpheus",
+        job: "leader"
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+
+    const jsonResponse = await response.json()
+    time = jsonResponse["createdAt"];
+    const minutes = time.split('T')[1].split(':')[1]
+    console.log(jsonResponse);
+    console.log(minutes);
+
     console.log(time);
     if (minutes == 29) {
       return true
-    } else { 
+    } else {
       return false
     }
-      
-    } 
-   
-    if (isLoading && isFileUploaded) {
-      return (
-        <>
-          <ReactLoading className = "reactLoading" color={"black"} height={50} width={100} />
-        </>
-      )
-    } else if (!isFileUploaded && !isLoading) {
-      return <div></div>
-   } else {
-      return(
-        <>
-          <p>
-            here we get the summarized text
-          </p>
-        </>
-      )
-    }
+
   }
+
+  if (isLoading && isFileUploaded) {
+    return (
+      <>
+        <ReactLoading className="reactLoading" color={"black"} height={50} width={100} />
+      </>
+    )
+  } else if (!isFileUploaded && !isLoading) {
+    return <div></div>
+  } else {
+    return (
+      <>
+        <p>
+          here we get the summarized text
+        </p>
+      </>
+    )
+  }
+}
 
 
 export default function Home() {
@@ -82,22 +82,22 @@ export default function Home() {
   return (
     <div className='container'>
       <span className='appName'>
-      <Image
-      src="/enabled-logo.svg"
-      width={300}
-      height={100}
-      alt="enabled logo"
-    />
-    <h4 className='subAppName'>Summarization app</h4>
+        <Image
+          src="/enabled-logo.svg"
+          width={300}
+          height={100}
+          alt="enabled logo"
+        />
+        <h4 className='subAppName'>Summarization app</h4>
       </span>
       <h3 className='heading'>
         Put here the pdf file you want to summarize.
       </h3>
       <Dashboard
         proudlyDisplayPoweredByUppy={false}
-				uppy={uppy}
-			/>
-      <Summarization/>
+        uppy={uppy}
+      />
+      <Summarization />
     </div>
   )
 }
