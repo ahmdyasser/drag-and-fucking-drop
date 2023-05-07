8d3ce14802861683ca2634a0f8cb2de1b5874b3e
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-05-07 20:34:06 +0300

    Add async code

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { Dashboard } from '@uppy/react'
 import Image from 'next/image';
 import '@uppy/core/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
+import { useState } from 'react';
+import ReactLoading from 'react-loading';
 
 const uppy = new Uppy({
   allowMultipleUploads: false,
@@ -12,11 +14,66 @@ const uppy = new Uppy({
 		endpoint: 'https://api2.transloadit.com'
 	});
 
-uppy.on('complete', (result) => {
-	console.log('Upload complete! Weve uploaded these files: ', result.successfful)
-})
+function Summarization() {
+  const [isLoading, setIsLoading] = useState(false);
+  uppy.on('complete', (result) => {
+    let timerId = setInterval(async function() {
+      if (await fetchStatus()) {
+        clearInterval(timerId);
+        setIsLoading(false);
+      }
+    }, 2000);
+    setIsLoading(true);
+    console.log('Upload complete! Weve uploaded these files: ', result.successfful);
+  })
+    
+  async function fetchStatus() {
+    var time;
+    let response = await fetch("https://reqres.in/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "morpheus",
+      job: "leader"
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  
+  const jsonResponse = await response.json()
+  time = jsonResponse["createdAt"];
+  const minutes = time.split('T')[1].split(':')[1]
+  console.log(jsonResponse);
+  console.log(minutes);
+  
+    console.log(time);
+    if (minutes == 29) {
+      return true
+    } else { 
+      return false
+    }
+      
+    } 
+   
+    if (isLoading) {
+      return (
+        <>
+          <ReactLoading className = "reactLoading" color={"black"} height={50} width={100} />
+        </>
+      )
+    }
+    return(
+      <>
+        <p>
+          here we get the summarized text
+        </p>
+      </>
+    )
+  }
+
 
 export default function Home() {
+
   return (
     <div className='container'>
       <span className='appName'>
@@ -25,10 +82,9 @@ export default function Home() {
       width={300}
       height={100}
       alt="enabled logo"
-    /> 
+    />
     <h4 className='subAppName'>Summarization app</h4>
       </span>
-      
       <h3 className='heading'>
         Put here the pdf file you want to summarize.
       </h3>
@@ -36,6 +92,7 @@ export default function Home() {
         proudlyDisplayPoweredByUppy={false}
 				uppy={uppy}
 			/>
+      <Summarization/>
     </div>
   )
 }
