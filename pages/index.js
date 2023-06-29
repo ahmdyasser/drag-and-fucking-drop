60d2078079d98ada04fbf791fdb768e1b96f782f
Mahmoud Hussein <mahmoudh3257@gmail.com>
2023-06-29 12:13:35 +0300

    Use flag to stop multiple requests to '/summarize'

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,51 @@
-import Uppy from '@uppy/core'
-import XHRUpload from '@uppy/xhr-upload'
-import { Dashboard } from '@uppy/react'
-import Image from 'next/image';
-import '@uppy/core/dist/style.css'
-import '@uppy/dashboard/dist/style.css'
-import { useState } from 'react';
-import ReactLoading from 'react-loading';
-import QuestionAndAnswer from '../components/QuestionAndAnswer';
+import Uppy from "@uppy/core";
+import XHRUpload from "@uppy/xhr-upload";
+import { Dashboard } from "@uppy/react";
+import Image from "next/image";
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import { useState, useEffect } from "react";
+import ReactLoading from "react-loading";
+import QuestionAndAnswer from "../components/QuestionAndAnswer";
 
 var isFileUploaded = false;
+var isSummarizationInProgress = false;
 const uppy = new Uppy({
   allowMultipleUploads: false,
-})
-  .use(XHRUpload, {
-    endpoint: 'http://127.0.0.1:8000/summarization/upload_file' // TODO: Update to our upload endpoint
-  });
+}).use(XHRUpload, {
+  endpoint: "http://127.0.0.1:8000/summarization/upload_file", // TODO: Update to our upload endpoint
+});
 
 function Summarization() {
   const [isLoading, setIsLoading] = useState(false);
   const [summarizedTextArray, setSummarizedTextArray] = useState([]);
-  uppy.on('complete', async (result) => {
-    console.log('Upload complete! Weve uploaded these files: ', result.successful);
-    isFileUploaded = true;
-    setIsLoading(true);
-    await getSummarization();
-    setIsLoading(false);
-  })
 
+  useEffect(() => {
+    uppy.on("complete", handleUploadComplete);
+  }, []);
+
+  async function handleUploadComplete(result) {
+    console.log(
+      "Upload complete! Weve uploaded these files: ",
+      result.successful
+    );
+    if (!isSummarizationInProgress) {
+      isSummarizationInProgress = true;
+      isFileUploaded = true;
+      setIsLoading(true);
+      await getSummarization();
+      setIsLoading(false);
+      isSummarizationInProgress = false;
+    }
+  }
   async function getSummarization() {
     let url = "http://127.0.0.1:8000/summarization/summarize";
     let response = await fetch(url, {
       method: "GET",
       headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
     const jsonResponse = await response.json();
     setSummarizedTextArray(jsonResponse.summary);
   }
@@ -42,11 +53,16 @@ function Summarization() {
   if (isLoading && isFileUploaded) {
     return (
       <>
-        <ReactLoading className="reactLoading" color={"black"} height={50} width={100} />
+        <ReactLoading
+          className="reactLoading"
+          color={"black"}
+          height={50}
+          width={100}
+        />
       </>
-    )
+    );
   } else if (!isFileUploaded && !isLoading) {
-    return <div></div>
+    return <div></div>;
   } else {
     return (
       <>
@@ -58,35 +74,30 @@ function Summarization() {
           ))}
         </ul>
       </>
-    )
+    );
   }
 }
 
-
 export default function Home() {
-
   return (
-    <div className='px-10 py-10 flex items-start'>
-      <div className='container'>
-        <span className='appName'>
+    <div className="px-10 py-10 flex items-start">
+      <div className="container">
+        <span className="appName">
           <Image
             src="/enabled-logo.svg"
             width={300}
             height={100}
             alt="enabled logo"
           />
-          <h4 className='subAppName'>Summarization app</h4>
+          <h4 className="subAppName">Summarization app</h4>
         </span>
-        <h3 className='font-sans text-xl font-bold'>
+        <h3 className="font-sans text-xl font-bold">
           Put here the pdf file you want to summarize.
         </h3>
-        <Dashboard
-          proudlyDisplayPoweredByUppy={false}
-          uppy={uppy}
-        />
+        <Dashboard proudlyDisplayPoweredByUppy={false} uppy={uppy} />
         <Summarization />
       </div>
       <QuestionAndAnswer />
     </div>
-  )
+  );
 }
